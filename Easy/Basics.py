35a62211a29ed47cb57988f48862cb37f2dc10dd
ryansimm <145355767+ryansimm@users.noreply.github.com>
2025-12-28 11:19:43 +0000

    Start

---
+++ Easy/Basics.py
@@ -0,0 +1,4 @@
+Name = str()
+Name = input("Enter your name: ")
+age = int(input("Please enter your age: "))
+print("Hello " + Name + ", you are " + str(age) + " years old.")
---
+++ README.txt
@@ -0,0 +1,3 @@
+The code within this repositary is a short general 
+workspace with various coding exercises to help further my 
+learning and consolidate my learning. 
