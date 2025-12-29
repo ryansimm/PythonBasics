c32470d7214223784fc28eed917cb95ffe9d4da2
ryansimm <145355767+ryansimm@users.noreply.github.com>
2025-12-29 03:14:45 +0000

    Easy basic python.

--- Easy/Basics.py
+++ Easy/Basics.py
@@ -1,4 +1,12 @@
 Name = str()
 Name = input("Enter your name: ")
-age = int(input("Please enter your age: "))
+age = float(input("Please enter your age: "))
 print("Hello " + Name + ", you are " + str(age) + " years old.")
+print("Age data type is " + str(type(age)))
+print("Name data type is " + str(type(Name)))
+if age <= 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+print("You will be 100 in " + str(100-age) + " years.")
